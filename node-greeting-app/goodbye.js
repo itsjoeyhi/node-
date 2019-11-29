ae6d9c136cdef592eebbf381c5f70aee98cdc14b
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-11-29 01:37:32 -0500

    Checkpoint for basic node app

---
+++ node-greeting-app/goodbye.js
@@ -0,0 +1,7 @@
+exports.goodbyeEnglish = function() {
+    console.log('Bye');
+}
+
+exports.greetingSpanish = function() {
+    console.log('Adios');
+}
---
+++ node-greeting-app/main.js
@@ -0,0 +1,15 @@
+const greetings = require('./greeting.js');
+
+const goodbyes = require('./goodbye.js');
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+goodbyes.goodbyeEnglish();
+
+goodbyes.greetingSpanish ();
